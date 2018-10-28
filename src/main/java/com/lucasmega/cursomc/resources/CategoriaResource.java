dacc94139306a3f84547e229b003f9f55b2f9b09
Lucas Mega <lucas.mega07@gmail.com>
2018-10-28 18:23:42 -0200

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/lucasmega/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lucasmega/cursomc/resources/CategoriaResource.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lucasmega.cursomc.doman.Categoria;
+import com.lucasmega.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
