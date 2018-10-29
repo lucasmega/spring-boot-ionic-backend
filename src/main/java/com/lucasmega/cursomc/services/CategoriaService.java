599e3ca6c35eb94b6e3f6275b7cbd53f5e4a2054
Lucas Mega <lucas.mega07@gmail.com>
2018-10-29 14:48:37 -0200

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/lucasmega/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucasmega/cursomc/services/CategoriaService.java
@@ -1,21 +1,23 @@
 package com.lucasmega.cursomc.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasmega.cursomc.domain.Categoria;
 import com.lucasmega.cursomc.repositories.CategoriaRepository;
-
-import java.util.Optional;
+import com.lucasmega.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + " Tipo: " + Categoria.class.getName()));
 	}
 }
